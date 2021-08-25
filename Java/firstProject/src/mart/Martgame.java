9cc78f74d27dcadbcc3078fbc6f9d5920b8c8dab
Hyun-Kim95 <75933619+Hyun-Kim95@users.noreply.github.com>
2021-08-25 13:54:25 +0900

    Add files via upload

--- Java/firstProject/src/mart/Martgame.java
+++ Java/firstProject/src/mart/Martgame.java
@@ -1,412 +1,219 @@
-//package mart;
-//
-//import java.util.Scanner;
-//
-//abstract class Appliances {
-//	public abstract void useAppliances();
-//}
-//
-//class Computer extends Appliances {
-//	String company;
-//	String type;
-//	int price;
-//
-//	public Computer(String company, String type, int price) {
-//		this.company = company;
-//		this.type = type;
-//		this.price = price;
-//	}
-//
-//	public String getcompany() {
-//		return company;
-//	}
-//
-//	public String gettype() {
-//		return type;
-//	}
-//
-//	public int getprice() {
-//		return price;
-//	}
-//
-//	@Override
-//	public void useAppliances() {
-//		System.out.println("제조사가 " + company + "인 " + type + "컴퓨터를 사용한다");
-//	}
-//}
-//
-//class Aircleaner extends Appliances {
-//	String company;
-//	int price;
-//
-//	public Aircleaner(String company, int price) {
-//		super();
-//		this.company = company;
-//		this.price = price;
-//	}
-//
-//	public String getcompany() {
-//		return company;
-//	}
-//
-//	public int getprice() {
-//		return price;
-//	}
-//
-//	@Override
-//	public void useAppliances() {
-//		System.out.println("제조사가 " + company + "인 " + "공기청정기가 공기를 깨끗하게 한다 ");
-//	}
-//}
-//
-//class Airconditioner extends Appliances {
-//	String company;
-//	String type;
-//	int price;
-//
-//	public Airconditioner(String company, String type, int price) {
-//		super();
-//		this.company = company;
-//		this.type = type;
-//		this.price = price;
-//	}
-//
-//	public String getcompany() {
-//		return company;
-//	}
-//
-//	public String gettype() {
-//		return type;
-//	}
-//
-//	public int getprice() {
-//		return price;
-//	}
-//
-//	@Override
-//	public void useAppliances() {
-//		System.out.println("제조사가 " + company + "인 " + type + "에어콘을 사용한다");
-//	}
-//}
-//
-//class Refrigerator extends Appliances {
-//	String company;
-//	String type;
-//	String capacity;
-//	int price;
-//
-//	public Refrigerator(String company, String type, String capacity, int price) {
-//		super();
-//		this.company = company;
-//		this.type = type;
-//		this.capacity = capacity;
-//		this.price = price;
-//	}
-//
-//	public String getcompany() {
-//		return company;
-//	}
-//
-//	public String gettype() {
-//		return type;
-//	}
-//
-//	public int getprice() {
-//		return price;
-//	}
-//
-//	@Override
-//	public void useAppliances() {
-//		System.out.println("제조사가 " + company + "인 " + type + "형 " + capacity + "냉장고가 식품을 관리한다");
-//	}
-//}
-//
-//class Mart {
-//	static int a = 60;
-//	private Computer[] StoredComputer;
-//	private Airconditioner[] StoredAirconditioner;
-//	private Refrigerator[] StoredRefrigerator;
-//	private Aircleaner[] StoredAircleaner;
-//
-//	public Mart() {
-//		StoredComputer = new Computer[10];
-//		StoredAirconditioner = new Airconditioner[10];
-//		StoredRefrigerator = new Refrigerator[10];
-//		StoredAircleaner = new Aircleaner[10];
-//	}
-//
-//	public void ready() {
-//		for (int i = 0; i < 5; i++) {
-//			StoredComputer[i] = new Computer("삼성", "i7", 200);
-//			StoredAirconditioner[i] = new Airconditioner("삼성", "벽걸이", 100);
-//			StoredRefrigerator[i] = new Refrigerator("삼성", "양문", "600L", 200);
-//			StoredAircleaner[i] = new Aircleaner("LG", 80);
-//		}
-//		for (int i = 5; i < 10; i++) {
-//			StoredComputer[i] = new Computer("LG", "i5", 150);
-//			StoredAirconditioner[i] = new Airconditioner("LG", "스탠드", 250);
-//			StoredRefrigerator[i] = new Refrigerator("LG", "4도어", "800L", 350);
-//			StoredAircleaner[i] = new Aircleaner("다이슨", 60);
-//		}
-//	}
-//
-//	public Appliances sellItem(int num, String companyname, Buyer guest) {
-//		Appliances Item;
-//		switch (num) {
-//		case 1:
-//			Item = searchComputer(companyname, guest);
-//			break;
-//		case 2:
-//			Item = searchAirconditioner(companyname, guest);
-//			break;
-//		case 3:
-//			Item = searchRefrigerator(companyname, guest);
-//			break;
-//		case 4:
-//			Item = searchAircleaner(companyname, guest);
-//			break;
-//		default:
-//			Item = null;
-//			break;
-//		}
-//		return Item;
-//	}
-//
-//	private Appliances searchComputer(String companyname, Buyer guest) {
-//		Appliances Item;
-//		for (int i = 0; i < StoredComputer.length; i++) {
-//			try
-//			{
-//				if (StoredComputer[i].getcompany().equals(companyname)) {
-//					if (guest.getMoney() < StoredComputer[i].getprice()) {
-//						return null;
-//					}
-//					else {
-//						Item = StoredComputer[i];
-//						guest.setMoney(guest.getMoney() - StoredComputer[i].getprice());
-//						StoredComputer[i] = null;
-//						return Item;
-//					}
-//				}
-//			} catch (Exception E) {}
-//		}
-//		return null;
-//	}
-//
-//	private Appliances searchAirconditioner(String companyname, Buyer guest) {
-//		Appliances Item;
-//		for (int i = 0; i < StoredAirconditioner.length; i++) {
-//			try
-//			{
-//				if (StoredAirconditioner[i].getcompany().equals(companyname)) {
-//					if (guest.getMoney() < StoredAirconditioner[i].getprice()) {
-//						return null;
-//					}
-//					else {
-//						Item = StoredAirconditioner[i];
-//						guest.setMoney(guest.getMoney() - StoredAirconditioner[i].getprice());
-//						StoredAirconditioner[i] = null;
-//						if(i == 4)
-//							Mart.a = 150;
-//						return Item;
-//					}
-//				}
-//			} catch (Exception e) {
-//			}
-//		}
-//		return null;
-//	}
-//
-//	private Appliances searchRefrigerator(String companyname, Buyer guest) {
-//		Appliances Item;
-//		for (int i = 0; i < StoredRefrigerator.length; i++) {
-//			try
-//			{
-//				if (StoredRefrigerator[i].getcompany().equals(companyname)) {
-//					if (guest.getMoney() < StoredRefrigerator[i].getprice()) {
-//						return null;
-//					}
-//					else {
-//						Item = StoredRefrigerator[i];
-//						guest.setMoney(guest.getMoney() - StoredRefrigerator[i].getprice());
-//						StoredRefrigerator[i] = null;
-//						return Item;
-//					}
-//
-//				}
-//			} catch (Exception e) {
-//			}
-//		}
-//		return null;
-//	}
-//
-//	private Appliances searchAircleaner(String companyname, Buyer guest) {
-//		Appliances Item;
-//
-//		for (int i = 0; i < StoredAircleaner.length; i++) {
-//			try
-//			{
-//				if (StoredAircleaner[i].getcompany().equals(companyname)) {
-//					if (guest.getMoney() < StoredAircleaner[i].getprice()) {
-//						return null;
-//					}
-//					else {
-//						Item = StoredAircleaner[i];
-//						guest.setMoney(guest.getMoney() - StoredAircleaner[i].getprice());
-//						StoredAircleaner[i] = null;
-//						if(i == 4)
-//							Mart.a = 80;
-//						else if(i==9)
-//							Mart.a = 100;
-//						return Item;
-//					}
-//				}
-//			} catch (Exception e) {
-//			}
-//		}
-//		return null;
-//	}
-//}
-//
-//class Buyer {
-//	private Appliances[] goods;
-//	private int money;
-//
-//	public Buyer(int money) {
-//		goods = new Appliances[20];
-//		this.money = money;
-//	}
-//
-//	public int getMoney() {
-//		return money;
-//	}
-//
-//	public void setMoney(int money) {
-//		this.money = money;
-//	}
-//
-//	public int buyItem(Appliances Item) {
-//		if (Item == null)
-//			return 0;
-//		for (int i = 0; i < goods.length; i++) {
-//			if (goods[i] == null) {
-//				goods[i] = Item;
-//				return 1;
-//			}
-//		}
-//		return 1;
-//	}
-//
-//	public void showMoney() {
-//		System.out.println("현재 잔액: " + money);
-//	}
-//
-//	public void showAppliances(int index) {
-//		for (int i = 0; i < goods.length; i++) {
-//			if (goods[i] != null) {
-//				System.out.print(index + "번째 소비자가 ");
-//				goods[i].useAppliances();
-//			} else
-//				return;
-//		}
-//	}
-//}
-//
-//public class Martgame {
-//	private static Mart mt;
-//	private static Buyer[] b;
-//	private static Scanner sc;
-//
-//	private static int num;
-//	private static int companynum;
-//	private static String companyname;
-//
-//	public static void main(String[] args) {
-//		mt = new Mart();
-//		b = new Buyer[3];
-//		for(int i=0;i<3;i++) {
-//			System.out.println((i+1)+"번 소비자의 돈을 입력하세요");
-//			b[i] = new Buyer(sc.nextInt());
-//		}
-//		mt.ready();
-//		sc = new Scanner(System.in);
-//		while (true) {
-//			for (int i = 0; i < b.length; i++) {
-//				if (b[i].getMoney() < 60)
-//					continue;
-//				System.out.println();
-//				System.out.print(i + 1 + "번 소비자 상품을 선택 해주세요.  현재잔액: ");
-//				System.out.println(b[i].getMoney());
-//				System.out.print("1.컴퓨터, 2.에어콘, 3.냉장고, 4.공기청정기 : ");
-//
-//				num = sc.nextInt();
-//				if ((num < 1) || (num > 4)) {
-//					System.out.println();
-//					System.out.println("잘못된 입력입니다.");
-//					System.out.println("다음사용자로 넘어갑니다.");
-//					System.out.println();
-//					sc.nextLine();
-//					continue;
-//				}
-//				sc.nextLine();
-//				System.out.println("제조사 및 사양을 선택해주세요");
-//				switch (num) {
-//				case 1:
-//				case 2:
-//				case 3:
-//					System.out.print("1.삼성, 2.LG : ");
-//					break;
-//				case 4:
-//					System.out.print("1.LG, 2.다이슨 : ");
-//					break;
-//				}
-//				companynum = sc.nextInt();
-//				if ((companynum < 1) || (companynum > 2)) {
-//					System.out.println();
-//					System.out.println("잘못된 입력입니다.");
-//					System.out.println("다음사용자로 넘어갑니다.");
-//					System.out.println();
-//					sc.nextLine();
-//					continue;
-//				}
-//				sc.nextLine();
-//				switch (num) {
-//				case 1:
-//				case 2:
-//				case 3:
-//					if (companynum == 1)
-//						companyname = "삼성";
-//					else
-//						companyname = "LG";
-//					break;
-//				case 4:
-//					if (companynum == 1) {
-//						companyname = "LG";
-//					}
-//					else {
-//						companyname = "다이슨";
-//					}
-//					break;
-//				}
-//				
-//				if(b[i].buyItem(mt.sellItem(num, companyname, b[i])) == 0) {
-//					System.out.println();
-//					System.out.println("구매 할 수 없습니다!! ");
-//					System.out.println("잔액 또는 상품의 재고가 부족합니다");
-//					System.out.println("다음사용자로 넘어갑니다.");
-//					System.out.println();
-//				}
-//				b[i].showMoney();
-//			}
-//			if ((b[0].getMoney() < Mart.a) && (b[1].getMoney() < Mart.a) && (b[2].getMoney() < Mart.a))
-//				break;
-//		}
-//		
-//		System.out.println("물건 사용");
-//		b[0].showAppliances(1);
-//		System.out.println();
-//		b[1].showAppliances(2);
-//		System.out.println();
-//		b[2].showAppliances(3);
-//	}
-//}
+package mart;
+
+import java.util.Scanner;
+
+class Computer {
+	String[] co = new String[10];
+
+	public Computer() {
+		for (int i = 0; i < 5; i++)
+			co[i] = "삼성 i7 200";
+		for (int i = 5; i < 10; i++)
+			co[i] = "LG i5 150";
+	}
+}
+
+class Aircon {
+	String[] ai = new String[10];
+
+	public Aircon() {
+		for (int i = 0; i < 5; i++)
+			ai[i] = "삼성 벽걸이 100";
+		for (int i = 5; i < 10; i++)
+			ai[i] = "LG 스탠드 250";
+	}
+}
+
+class Fresh {
+	String[] fr = new String[10];
+
+	public Fresh() {
+		for (int i = 0; i < 5; i++)
+			fr[i] = "삼성 양문 600L 200";
+		for (int i = 5; i < 10; i++)
+			fr[i] = "LG 4도어 800L 350";
+	}
+}
+
+class Clean {
+	String[] cl = new String[10];
+
+	public Clean() {
+		for (int i = 0; i < 5; i++)
+			cl[i] = "LG 80";
+		for (int i = 5; i < 10; i++)
+			cl[i] = "다이슨 60";
+	}
+}
+
+class Mart {
+	static Scanner sc;
+	static Computer co;
+	static Aircon ai;
+	static Fresh fr;
+	static Clean cl;
+
+	public Mart() {
+		sc = new Scanner(System.in);
+		co = new Computer();
+		ai = new Aircon();
+		fr = new Fresh();
+		cl = new Clean();
+	}
+
+	public void shopping(Buyer bu) {
+		System.out.println("번 소비자님 상품을 선택해 주세요 1.컴퓨터 2.에어컨 3.냉장고 4.공기청정기");
+		int a = sc.nextInt();
+		switch (a) {
+		case 1:
+		case 2:
+		case 3:
+			System.out.println("제조사를 선택해주세요 1.삼성 2.LG");
+			break;
+		case 4:
+			System.out.println("제조사를 선택해주세요 1.LG 2.다이슨");
+			break;
+		default:
+			System.out.println("잘못된 입력입니다.");
+			break;
+		}
+		if (a > 4 || a < 1)
+			return;
+		int b = sc.nextInt();
+		if (b < 1 || b > 2) {
+			System.out.println("잘못된 입력입니다.");
+			return;
+		}
+
+		bu.buy(a, b);
+	}
+}
+
+class Buyer {
+	String[] bag;
+	int money;
+
+	public Buyer(int money) {
+		this.money = money;
+		bag = new String[20];
+	}
+
+	public void buy(int a, int b) {
+		switch (a) {
+		case 1:
+			goods("Computer ", Mart.co.co, b);
+			break;
+		case 2:
+			goods("Aircon ", Mart.ai.ai, b);
+			break;
+		case 3:
+			goods("Fresh ", Mart.fr.fr, b);
+			break;
+		case 4:
+			goods("Clean ", Mart.cl.cl, b);
+			break;
+		default:
+			System.out.println("제대로 입력해주세요.");
+		}
+	}
+
+	public void goods(String k, String[] a, int b) {
+		for (int i = 0; i < 20; i++) {
+			if (bag[i] == null) {
+				if (b == 1) {
+					for (int j = 0; j < 5; j++) {
+						if (a[j] != null) {
+							bag[i] = k + a[j];
+							String[] aj = a[j].split(" ");
+							if (Integer.parseInt(aj[aj.length - 1]) > money) {
+								System.out.println("금액이 부족합니다.");
+								break;
+							}
+							money -= Integer.parseInt(aj[aj.length - 1]);
+							a[j] = null;
+							break;
+						}
+						if (j == 4) {
+							System.out.println("상품이 없습니다.");
+							break;
+						}
+					}
+				} else {
+					for (int j = 5; j < 10; j++) {
+						if (a[j] != null) {
+							bag[i] = k + a[j];
+							String[] aj = a[j].split(" ");
+							if (Integer.parseInt(aj[aj.length - 1]) > money) {
+								System.out.println("금액이 부족합니다.");
+								break;
+							}
+							money -= Integer.parseInt(aj[aj.length - 1]);
+							a[j] = null;
+							break;
+						}
+						if (j == 9) {
+							System.out.println("상품이 없습니다.");
+							break;
+						}
+					}
+				}
+				System.out.println("남은 금액: " + money);
+				break;
+			}
+		}
+	}
+
+	public void use(int num) {
+		for (int i = 0; i < 20; i++) {
+			if (bag[i] == null)
+				break;
+			System.out.print(num + "번 소비자가 ");
+			String[] k = bag[i].split(" ");
+			switch (k[0]) {
+			case "Computer":
+				System.out.println("제조사가 " + k[1] + "인 " + k[2] + "컴퓨터를 사용한다");
+				break;
+			case "Aircon":
+				System.out.println("제조사가 " + k[1] + "인 " + k[2] + "에어콘을 사용한다");
+				break;
+			case "Fresh":
+				System.out.println("제조사가 " + k[1] + "인 " + k[2] + "형 " + k[3] + "냉장고가 식품을 관리한다");
+				break;
+			case "Clean":
+				System.out.println("제조사가 " + k[1] + "인 공기청정기가 공기를 깨끗하게 한다");
+			}
+		}
+	}
+}
+
+public class Martgame {
+	static Scanner sc;
+	static Buyer[] b;
+	static Mart m = new Mart();
+
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		b = new Buyer[3];
+		System.out.println("마트게임 시작");
+		for (int i = 0; i < 3; i++) {
+			System.out.print(i + 1 + "번 소비자의 돈을 입력하세요 : ");
+			b[i] = new Buyer(sc.nextInt());
+		}
+		while (true) {
+			for (int i = 0; i < b.length; i++) {
+				if (b[i].money < 60)
+					continue;
+				System.out.print(i + 1);
+				m.shopping(b[i]);
+			}
+			if (b[0].money < 60 && b[1].money < 60 && b[2].money < 60) {
+				break;
+			}
+		}
+		for (int j = 0; j < 3; j++) {
+			b[j].use(j + 1);
+			System.out.println();
+		}
+		System.out.println("마트게임 종료");
+	}
+}
