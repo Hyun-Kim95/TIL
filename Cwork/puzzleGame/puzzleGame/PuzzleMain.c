a54a60de28620f06d2fdf356f0db3468ac850592
Hyun-Kim95 <75933619+Hyun-Kim95@users.noreply.github.com>
2021-09-08 17:54:09 +0900

    Add files via upload

---
+++ Cwork/puzzleGame/puzzleGame/PuzzleMain.c
@@ -0,0 +1,9 @@
+//PuzzleMain.c
+#include "Rangking.h"
+extern int playFifteenPuzzle();
+void main() {
+	loadRanking("ranking.txt");
+	int rank = playFifteenPuzzle();
+	printRanking();
+	storeRanking("ranking.txt");
+}
